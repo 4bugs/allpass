265591aa373bf1a90a485f46c642216e3fc2c3a4
Profeel <cqupt_wq@hotmail.com>
2017-06-07 13:31:13 +0800

    未添加URL

--- pass/admin.py
+++ pass/admin.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import PasswordItems, Manager
+from models import PasswordItems, PasswordGroup, Manager
 # Register your models here.
 
 admin.site.register(PasswordItems)
+admin.site.register(PasswordGroup)
 admin.site.register(Manager)
--- pass/views.py
+++ pass/views.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Manager,PasswordItems
 
-# Create your views here.
+class PasswordView(generic.DetailView):
+    model = PasswordItems
+    template_name = 'pass/password.html'
